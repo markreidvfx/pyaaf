e9ab8b8a4d46583d295515ad4529e30c627a6975
Mark <mindmark@gmail.com>
2013-09-03 16:34:52 -0700

    added dump example

---
+++ example/dump.py
@@ -0,0 +1,34 @@
+import aaf
+import traceback
+from optparse import OptionParser
+    
+parser = OptionParser()
+
+(options, args) = parser.parse_args()
+
+if not args:
+    parser.error("not enough args")
+
+
+
+def walk_properties(space, iter_item):
+    for item in iter_item:
+        value = item
+        if isinstance(item, aaf.property.Property):
+            value = item.value
+        name = ""
+        if hasattr(item, 'name'):
+            name = item.name or ""
+        
+        print space,name, value
+        s = space + '   '
+        if isinstance(value, aaf.base.AAFObject):
+            walk_properties(s, value.properties())
+        if isinstance(value, aaf.iterator.BaseIterator):
+            walk_properties(s, value)
+
+
+
+f = aaf.open(args[0])
+
+walk_properties("", f.header().properties())
