8b1228729d4b83bdb9338dca85032d9fc0dab7ca
Mark <mindmark@gmail.com>
2013-09-04 16:27:24 -0700

    added aaf2xml example

---
+++ example/aaf2xml.py
@@ -0,0 +1,17 @@
+import aaf
+import os
+from optparse import OptionParser
+    
+parser = OptionParser()
+(options, args) = parser.parse_args()
+
+if not args:
+    parser.error("not enough argements")
+    
+path = args[0]
+name, ext = os.path.splitext(path)
+
+f = aaf.open(path, 'r')
+f.save(name + ".xml")
+f.close()
+
