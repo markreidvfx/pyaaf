41c69089d47cad78ed04074638e859d7408dc463
mark reid <mindmark@gmail.com>
2014-06-05 10:31:41 -0700

    added __init__

--- aaf/define/TaggedValueDef.pyx
+++ aaf/define/TaggedValueDef.pyx
@@ -18,3 +18,15 @@ cdef class TaggedValueDef(DefObject):
     def __dealloc__(self):
         if self.ptr:
             self.ptr.Release()
+            
+    def __init__(self, root, auid, name, description):
+        cdef Dictionary dictionary = root.dictionary
+        dictionary.create_instance(self)
+        
+        cdef AUID auid_obj = AUID(auid)
+        
+        cdef AAFCharBuffer name_buf = AAFCharBuffer(name)
+        cdef AAFCharBuffer description_buf = AAFCharBuffer(name)
+
+        error_check(self.ptr.Initialize(auid_obj.get_auid(), name_buf.get_ptr(), description_buf.get_ptr()))
+    
