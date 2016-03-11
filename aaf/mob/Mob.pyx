60a67d80a7fde94899a5a82fa8f6e6beaac83a79
Mark Reid <mindmark@gmail.com>
2016-03-11 12:56:29 -0800

    cleaned up whitespace

--- aaf/mob/Mob.pyx
+++ aaf/mob/Mob.pyx
@@ -2,7 +2,7 @@
 cdef class Mob(AAFObject):
     """Base Class for All Mob Objects
     """
-    
+
     def __cinit__(self):
         self.iid = lib.IID_IAAFMob
         self.auid = lib.AUID_AAFMob
@@ -10,31 +10,31 @@ cdef class Mob(AAFObject):
 
     cdef lib.IUnknown **get_ptr(self):
         return <lib.IUnknown **> &self.ptr
-    
+
     cdef query_interface(self, AAFBase obj = None):
         if obj is None:
             obj = self
         else:
             query_interface(obj.get_ptr(), <lib.IUnknown**>&self.ptr, lib.IID_IAAFMob)
-            
+
         if not self.mob2_ptr:
             query_interface(obj.get_ptr(), <lib.IUnknown**>&self.mob2_ptr, lib.IID_IAAFMob2)
-            
+
         AAFObject.query_interface(self, obj)
-            
+
     def __dealloc__(self):
         if self.ptr:
             self.ptr.Release()
-        
+
         if self.mob2_ptr:
             self.mob2_ptr.Release()
-            
+
     def slots(self):
         cdef MobSlotIter slot_iter = MobSlotIter.__new__(MobSlotIter)
-        error_check(self.ptr.GetSlots(&slot_iter.ptr)) 
-        slot_iter.root = self.root   
+        error_check(self.ptr.GetSlots(&slot_iter.ptr))
+        slot_iter.root = self.root
         return slot_iter
-    
+
     def slot_at(self, lib.aafSlotID_t slotID):
         """slot_at(slotID)
         """
@@ -42,7 +42,7 @@ cdef class Mob(AAFObject):
             if slot.slotID == slotID:
                 return slot
         raise IndexError("Invalid slot number: %d" % slotID)
-    
+
     def insert_slot(self, lib.aafUInt32 index, MobSlot slot):
         """insert_slot(index, slot)
         Inserts the given slot into this mob at the given index.  All
@@ -50,50 +50,50 @@ cdef class Mob(AAFObject):
         index to accommodate.
         """
         error_check(self.ptr.InsertSlotAt(index, slot.slot_ptr))
-        
+
     def append_slot(self, MobSlot slot not None):
         error_check(self.ptr.AppendSlot(slot.slot_ptr))
-        
+
     def create_clip(self, slotID = None, length = None, start_time = None):
         """create_clip(slotID = None, length = None, start_time = None)
         """
 
         d = self.dictionary()
-        
+
         if slotID is None:
             slotID = list(self.slots())[0].slotID
-            
+
         source_slot = self.slot_at(slotID)
-        
+
         if length is None:
             length = source_slot.segment.length
-        
+
         if start_time is None:
             start_time = source_slot.origin
-            
+
         cdef SourceRef source_ref = SourceRef(self.mobID, slotID, start_time)
-        
+
         return d.create.SourceClip(source_slot.media_kind, length, source_ref)
-        
-    def append_new_timeline_slot(self, edit_rate, Segment seg, lib.aafSlotID_t slotID = 0, 
+
+    def append_new_timeline_slot(self, edit_rate, Segment seg, lib.aafSlotID_t slotID = 0,
                                  slot_name = None, lib.aafPosition_t origin = 0):
-        
+
         """append_new_timeline_slot(edit_rate, seg, slotID = 0, slot_name = None, origin = 0)
-        
+
         Creates a new :class:`TimelineMobSlot` with the given property values and appends it to the :class:`Mob`
         """
-        
+
         if not slot_name:
             slot_name = 'timeline slot %d' % slotID
-        
+
         cdef TimelineMobSlot timeline = TimelineMobSlot.__new__(TimelineMobSlot)
-        
+
         cdef lib.aafRational_t edit_rate_t
-        
+
         fraction_to_aafRational(edit_rate, edit_rate_t)
 
         cdef AAFCharBuffer slot_name_buf = AAFCharBuffer(slot_name)
-        
+
         error_check(self.ptr.AppendNewTimelineSlot(edit_rate_t,
                                                   seg.seg_ptr,
                                                   slotID,
@@ -104,16 +104,16 @@ cdef class Mob(AAFObject):
         timeline.query_interface()
         timeline.root = self.root
         return timeline
-    
+
     def append_comment(self, name not None, value not None):
         """append_comment(name, value)
         """
-        
+
         cdef AAFCharBuffer name_buf = AAFCharBuffer(name)
         cdef AAFCharBuffer value_buf = AAFCharBuffer(value)
 
         error_check(self.ptr.AppendComment(name_buf.get_ptr(), value_buf.get_ptr() ))
-    
+
     def comments(self):
         cdef TaggedValueIter tags = TaggedValueIter.__new__(TaggedValueIter)
         tags.root = self.root
@@ -122,7 +122,7 @@ cdef class Mob(AAFObject):
             return []
         else:
             error_check(hr)
-        
+
         return tags
 
     def comment_dict(self):
@@ -130,43 +130,43 @@ cdef class Mob(AAFObject):
         for item in self.comments():
             d[item.name] = item.value
         return d
-    
+
     def remove_comment_by_name(self, name not None):
         """remove_comment_by_name(name)
         """
-        
+
         cdef TaggedValue tag
-        
+
         for tag in self.comments():
             if tag.name == name:
                 error_check(self.ptr.RemoveComment(tag.ptr))
                 return
         raise KeyError("No comment with name: %s" % str(name))
-    
+
     def remove_comment(self, TaggedValue tag not None):
         """remove_comment(tag)
         """
         error_check(self.ptr.RemoveComment(tag.ptr))
-        
+
     def append_attribute(self, name not None, value not None):
         """append_attribute(name, value):
         """
-        
+
         cdef AAFCharBuffer name_buf = AAFCharBuffer(name)
         cdef AAFCharBuffer value_buf = AAFCharBuffer(value)
 
         error_check(self.mob2_ptr.AppendAttribute(name_buf.get_ptr(), value_buf.get_ptr() ))
-        
+
         for item in self['Attributes'].value:
             if item.name == name:
                 return item
-    
+
     def __richcmp__(x, y, int op):
         if op == 2:
-            
+
             if isinstance(x, Mob):
                 x = x.mobID
-            
+
             if isinstance(y, Mob):
                 y = y.mobID
 
@@ -174,24 +174,24 @@ cdef class Mob(AAFObject):
                 return True
             return False
         raise NotImplemented("richcmp %d not not Implemented" % op)
-        
+
     def __repr__(self):
         name = self.name
         if name:
             return '<%s.%s %s %s at 0x%x>' % (
                 self.__class__.__module__,
                 self.__class__.__name__,
-                name, str(self.mobID), 
+                name, str(self.mobID),
                 id(self),
                 )
         else:
             return '<%s.%s %s at 0x%x>' % (
                 self.__class__.__module__,
                 self.__class__.__name__,
-                str(self.mobID), 
+                str(self.mobID),
                 id(self),
                 )
-    
+
     property name:
         def __get__(self):
             for p in self.properties():
@@ -201,11 +201,11 @@ cdef class Mob(AAFObject):
                         return name
 
             return None
-        
+
         def __set__(self, value):
             cdef AAFCharBuffer name_buf = AAFCharBuffer(value)
             error_check(self.ptr.SetName(name_buf.get_ptr()))
-            
+
     property nb_slots:
         def __get__(self):
             cdef lib.aafNumSlots_t nb_slots
@@ -238,12 +238,10 @@ cdef class Mob(AAFObject):
             cdef lib.aafMobID_t mobID
             error_check(self.ptr.GetMobID(&mobID))
             cdef MobID mobID_obj = MobID()
-            
+
             mobID_obj.mobID = mobID
             return mobID_obj
-        
+
         def __set__(self, value):
             cdef MobID mobID_obj = MobID(value)
             error_check(self.ptr.SetMobID(mobID_obj.get_aafMobID_t()))
-            
-          
--- tests/test_storage.py
+++ tests/test_storage.py
@@ -25,41 +25,41 @@ if not os.path.exists(sandbox):
 main_test_file = os.path.join(cur_dir,"files/test_file_01.aaf")
 
 class TestFile(unittest.TestCase):
-    
+
     def test_lookup(self):
         test_mobID = "urn:smpte:umid:060a2b34.01010101.01010f00.13000000.060e2b34.7f7f2a80.48c9f1f4.2abb0184"
-        
+
         f = aaf.open(main_test_file)
-        
+
         mob = f.storage.lookup_mob(test_mobID)
-        
+
         assert mob.mobID == test_mobID
-    
+
     def test_lookup_richcmp(self):
         f = aaf.open(main_test_file)
-        
+
         for mob in f.storage.mobs():
             assert mob == f.storage.lookup_mob(mob.mobID)
             assert mob == f.storage.lookup_mob(mob.mobID).mobID
-            
+
     def test_auid(self):
-        
+
         for x in range(10):
             u = uuid.uuid4()
             auid = aaf.util.AUID(u)
             assert auid == u
             assert u == auid
             assert u == auid.to_UUID()
-            
-            
+
+
         smpte_ul = aaf.util.AUID.from_urn_smpte_ul("urn:smpte:ul:060e2b34.01040101.0e040301.02000000")
         print("urn:smpte:ul:060e2b34.01040101.0e040301.02000000")
         print(smpte_ul.to_urn_smpte_ul())
         print("   ",smpte_ul)
-        
+
         assert smpte_ul.to_urn_smpte_ul() == "urn:smpte:ul:060e2b34.01040101.0e040301.02000000"
-        
-        
-        
+
+
+
 if __name__ == '__main__':
     unittest.main()
