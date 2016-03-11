db12755dfe15c620b0f30009d6e0105bfcd40bc1
Mark Reid <mindmark@gmail.com>
2016-03-11 12:25:19 -0800

    cleaned up whitespace

--- aaf/base.pyx
+++ aaf/base.pyx
@@ -2,7 +2,7 @@
 cimport lib
 from .util cimport error_check, query_interface, resolve_object, AUID
 
-from .define cimport ClassDef, PropertyDef 
+from .define cimport ClassDef, PropertyDef
 from .iterator cimport PropIter, PropItemIter
 from .property cimport PropertyValue, PropertyItem
 #from .resolve import resolve_object
@@ -14,66 +14,66 @@ cdef class AAFBase(object):
         self.base_ptr = NULL
         self.iid = lib.IID_IUnknown
         self.root = None
-          
+
     def __init__(self, *args, **kwargs):
         raise TypeError("%s cannot be instantiated from Python" %  self.__class__.__name__)
 
     cdef lib.IUnknown **get_ptr(self):
         return &self.base_ptr
-    
+
     cdef query_interface(self, AAFBase obj = None):
         if obj is None:
             return
         query_interface(obj.get_ptr(), &self.base_ptr, lib.IID_IUnknown)
-    
+
     cdef resolve(self):
         return resolve_object(self)
 
     def __dealloc__(self):
         if self.base_ptr:
             self.base_ptr.Release()
-            
+
         self.root = None
-            
+
     property class_iid:
         def __get__(self):
             cdef AUID auid = AUID()
             auid.from_iid(self.iid)
             return auid
-            
-            
+
+
 
 cdef class AAFObject(AAFBase):
     def __cinit__(self):
         self.obj_ptr = NULL
         self.iid = lib.IID_IAAFObject
         self.auid = lib.AUID_AAFObject
-    
+
     cdef lib.IUnknown **get_ptr(self):
         return <lib.IUnknown **> &self.obj_ptr
-    
+
     cdef query_interface(self, AAFBase obj = None):
         if obj is None:
             obj = self
         else:
             query_interface(obj.get_ptr(), <lib.IUnknown **> &self.obj_ptr, lib.IID_IAAFObject)
-            
+
         AAFBase.query_interface(self, obj)
-        
+
     def __dealloc__(self):
         if self.obj_ptr:
             self.obj_ptr.Release()
-        
+
     def __getitem__(self, x):
-        
+
         for p in self.properties():
             if p.name == x:
-                
+
                 return p
-            
+
         cdef PropertyItem item
         cdef PropertyDef propdef
-        cdef AAFObject new_obj 
+        cdef AAFObject new_obj
         classdef = self.classdef()
         for propdef in classdef.all_propertydefs():
             if propdef.name == x:
@@ -84,19 +84,19 @@ cdef class AAFObject(AAFBase):
                 item.parent = new_obj
                 item.root = self.root
                 return item
-            
+
         raise KeyError("Key %s not found" % x)
-    
+
     def keys(self):
         """
         Return a list of the AAFObjects property names
         """
         return [p.name for p in self.properties()]
-    
+
     def all_keys(self):
-        
+
         return [p.name for p in self.classdef().all_propertydefs()]
-    
+
     def has_key(self, key):
         """
         Test for the presence of key in the AAFObject property names
@@ -104,25 +104,25 @@ cdef class AAFObject(AAFBase):
         if key in self.keys():
             return True
         return False
-    
+
     def get(self, key, default=None):
         """
-        Return the property object for key if key is in the AAFObject, else default. 
+        Return the property object for key if key is in the AAFObject, else default.
         If default is not given, it defaults to None, so that this method never raises a KeyError.
         """
         if key in self.all_keys():
             return self[key]
         return default
-    
+
     def get_value(self, key, default=None):
         """
-        Return the property value for key if key is in the AAFObject, else default. 
+        Return the property value for key if key is in the AAFObject, else default.
         If default is not given, it defaults to None, so that this method never raises a KeyError.
         """
         if self.has_key(key):
             return self[key].value
         return default
-    
+
     def dictionary(self):
         """
         Returns the Dictionary Object the AAFObject belongs to.
@@ -132,7 +132,7 @@ cdef class AAFObject(AAFBase):
         d.query_interface()
         d.root = self.root
         return d
-    
+
     def classdef(self):
         """
         Returns the Class Definition
@@ -142,16 +142,16 @@ cdef class AAFObject(AAFBase):
         class_def.query_interface()
         class_def.root = self.root
         return class_def
-    
+
     def properties(self):
         """
         Returns a property Iterator
         """
-        
+
         cdef PropItemIter prop_iter = PropItemIter.__new__(PropItemIter)
         cdef AAFObject new_obj = AAFObject.__new__(AAFObject)
         new_obj.query_interface(self)
-        
+
         error_check(self.obj_ptr.GetProperties(&prop_iter.ptr))
         prop_iter.parent = new_obj
         prop_iter.root = self.root
@@ -163,7 +163,7 @@ cdef class AAFObject(AAFBase):
             return '<%s.%s %s at 0x%x>' % (
                     self.__class__.__module__,
                     self.__class__.__name__,
-                    str(name), 
+                    str(name),
                     id(self),
                     )
         else:
@@ -172,8 +172,8 @@ cdef class AAFObject(AAFBase):
                     self.__class__.__name__,
                     id(self),
                     )
-    
-    
+
+
     property class_auid:
         """
         The AUID of the AAFObject
@@ -182,7 +182,7 @@ cdef class AAFObject(AAFBase):
             cdef AUID auid = AUID()
             auid.from_auid(self.auid)
             return auid
-        
+
     property name:
         """
         The name of the AAFObject, None if Object doesn't have a "Name" property.
@@ -194,7 +194,7 @@ cdef class AAFObject(AAFBase):
                     if name:
                         return name
             return None
-    
+
     property class_name:
         """
         Resolved class name for AAFOBject from its ClassDef.
--- tests/test_iterator.py
+++ tests/test_iterator.py
@@ -32,98 +32,98 @@ def iter_count(iterable):
         result = next(iterable, None)
         if not result:
             break
-        i += 1 
-    return i   
+        i += 1
+    return i
 
 
 
 class TestFile(unittest.TestCase):
-    
+
     def test_len(self):
         f = aaf.open(main_test_file)
         iterable = f.storage.mobs()
-        
+
         assert len(iterable) == 199
-    
-        count = 0 
+
+        count = 0
         for i, item in enumerate(iterable):
             #print item
             assert len(iterable) == 199
             count += 1
-            
+
         assert count == len(iterable)
         assert len(iterable) == 199
-        
+
     def test_negative_index(self):
         f = aaf.open(main_test_file)
         iterable = f.storage.mobs()
-        
-        
+
+
         last_item = iterable[len(iterable)-1]
         assert last_item == iterable[-1]
-        
+
         try:
             iterable[-10000]
         except IndexError:
             pass
         else:
             raise
-        
+
         for i in range(len(iterable)):
             assert iterable[i] == iterable[i-len(iterable)]
-            
+
     def test_slice(self):
         f = aaf.open(main_test_file)
         iterable = f.storage.mobs()
-        
-        
+
+
         s = iterable[1:10]
         l = []
         for i in range(1, 10):
             l.append(iterable[i])
-        
+
         assert s == l
-        
+
         s = iterable[100:-10:2]
-        
+
         l = []
-        
+
         for i in range(100, len(iterable)-10, 2):
             l.append(iterable[i])
-        
+
         assert s == l
-        
+
         l = iterable[-1000: 1000]
-        
-        
+
+
     def test_skip(self):
         f = aaf.open(main_test_file)
         iterable = f.storage.mobs()
-        
+
         assert len(iterable) == 199
-        
-        
+
+
         skip_amount = 100
         iterable.skip(skip_amount)
-        
+
         assert iter_count(iterable) == 199 - skip_amount
-        
+
         iterable.reset()
-        
+
         assert len(iterable) == 199
-        
-        
+
+
         iterable.skip(skip_amount)
-        
+
         try:
             iterable.skip(1000)
         except IndexError:
             pass
         else:
             raise
-        
+
         assert iter_count(iterable) == 199 - skip_amount
-            
-        
+
+
 if __name__ == '__main__':
     unittest.main()
