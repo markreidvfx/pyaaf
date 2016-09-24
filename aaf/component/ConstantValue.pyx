d501ddaa8b3ac306f8973ffeb32a35cc14964bbf
Mark Reid <mindmark@gmail.com>
2016-09-24 11:44:00 -0700

    add __init__ to ConstantValue

--- aaf/component/ConstantValue.pyx
+++ aaf/component/ConstantValue.pyx
@@ -18,3 +18,23 @@ cdef class ConstantValue(Parameter):
     def __dealloc__(self):
         if self.ptr:
             self.ptr.Release()
+
+    def __init__(self, root, ParameterDef param_def not None, unsigned char[:] data not None):
+
+        cdef Dictionary dictionary = root.dictionary
+        dictionary.create_instance(self)
+
+        error_check(self.ptr.Initialize(param_def.ptr, len(data), &data[0]))
+
+    property data:
+        def __get__(self):
+            cdef lib.aafUInt32 size
+            cdef lib.aafUInt32 bytes_read
+            error_check(self.ptr.GetValueBufLen(&size))
+            buf  = bytearray(size)
+            cdef unsigned char[:] data = buf
+            error_check(self.ptr.GetValue(size, &data[0], &bytes_read))
+            assert bytes_read == size
+            return buf
+        def __set__(self, unsigned char[:] data not None):
+            error_check(self.ptr.SetValue(len(data), &data[0]))
